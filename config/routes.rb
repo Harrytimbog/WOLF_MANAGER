88fe15e6745dfbc682bf1cc5a94565e9875dec52
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-04-28 15:49:01 +0100

    create projects and tasks controller actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :names
-  resources :projects
+  resources :projects do
+    resources :tasks
+  end
   devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "projects#index"
 end
