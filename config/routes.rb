e0e65cb6db8cd4676ed0ca10374ce18ad7aab0a6
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-05-31 12:05:48 +0100

    Add a bit of style to pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "projects#index"
+  root to: "pages#home"
 end
