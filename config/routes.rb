772956d1900b56f88ece3b3fb962bec1109d2780
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-07-03 19:52:28 +0100

    set favicon and meta tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+  match "/422", :to => "errors#unprocessable_entity", :via => :all
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :projects do
     resources :tasks
